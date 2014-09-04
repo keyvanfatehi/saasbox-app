62548e1a3cf668650ddfa38a3af94c676a391f09
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-09-04 03:25:29 -0700

    fix react load error

--- src/client/app.js
+++ src/client/app.js
@@ -1,6 +1,9 @@
 /** @jsx React.DOM */
-if (process.env.NODE_ENV) {
-  window.React = require('react')
+var React = require('react')
+
+// Load React Chrome Dev Tools in development
+if (process.env.NODE_ENV === 'development') {
+  window.React = React
 }
 
 var Landing = require('./components/landing');
