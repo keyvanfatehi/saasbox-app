958a48aa7145c6a4917b7430a4fcb1e9fc039995
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-09-04 22:48:20 -0700

    use async to seed against all agents before listen

--- server.js
+++ server.js
@@ -2,27 +2,21 @@ var logger = require('winston')
 logger.remove(logger.transports.Console);
 logger.add(logger.transports.Console, { colorize: true });
 
-var app = require(__dirname+'/src/server/app.js');
-var port = process.env.PORT || 3000;
-
-logger.info('seeding the product...')
-var product = require('./product')
 var config = require('./etc/config')
-var _ = require('lodash')
-var Agent = require('./src/server/agent')
+  , async = require('async')
+  , Agent = require('./src/server/agent')
+  , app = require(__dirname+'/src/server/app.js')
+  , port = process.env.PORT || 3000
+  
+function seedAgent(agentConfig, cb) {
+  var product = require('./product')
+  var agent = new Agent(agentConfig)
+  logger.info("seeding "+product.slug+" on "+agent.name)
+  agent.defineProduct(product, cb)
+}
 
-_.each(config.agents, function(agentConfig) {
-  var agent = new Agent(agentConfig);
-  agent.route('/drops/'+product.slug, function(options) {
-    options.headers['Content-Type'] = 'application/javascript'
-    options.scheme = "http"
-    options.host = "localhost"
-    options.port = "4000"
-  }).post(product.ydm, function(err, res) {
-    if (err)
-      throw new Error('Failed to seed '+product.slug+': '+err.message)
-  }).end()
+async.each(config.agents, seedAgent, function(err) {
+  if (err) throw err;
+  require('http').Server(app).listen(port, '0.0.0.0');
+  logger.info("listening on http://0.0.0.0:"+port);
 })
-
-require('http').Server(app).listen(port, '0.0.0.0');
-logger.info("listening on http://0.0.0.0:"+port);
--- src/server/agent.js
+++ src/server/agent.js
@@ -1,7 +1,8 @@
 var Resource = require('../resource')
-  , URI = require('uri-js');
+  , URI = require('uri-js')
 
 var Agent = function (agentConfig) {
+  this.name = agentConfig.name;
   this.configure = function(options) {
     var uri = URI.parse(agentConfig.url);
     options.headers['X-Auth-Token'] = agentConfig.secret
@@ -25,6 +26,15 @@ Agent.prototype = {
     var req = this.route('/drops/'+instance.slug+'/'+action).post({
       namespace: instance.namespace
     }, cb).end()
+  },
+  defineProduct: function(product, cb) {
+    this.route('/drops/'+product.slug, function(options) {
+      options.headers['Content-Type'] = 'application/javascript'
+    }).post(product.ydm, function(err, res) {
+      if (err) cb(err);
+      if (res.statusCode === 201) cb(null, res);
+      else cb(new Error(res.statusCode+' '+res.body))
+    }).end()
   }
 }
 
