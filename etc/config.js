5b555f20375306d46e6abe22d4b5231177adcc3e
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-09-05 05:08:05 -0700

    setup production

--- etc/config.js
+++ etc/config.js
@@ -7,5 +7,12 @@ module.exports = {
       secret: 'secret'
     }]
   },
-  production: {}
+  production: {
+    product: 'strider',
+    agents: [{
+      name: 'the-agency',
+      url: 'https://agency.knban.com',
+      secret: 'ryxvmm876h2maemi2zh1bv1ntpujtt9zw2o0vgu3hy6i529t7fz9mrcva0dx6r'
+    }]
+  }
 }[process.env.NODE_ENV || 'development']
