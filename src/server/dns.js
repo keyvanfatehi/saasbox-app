d0a7bab24f0ec53915dc672a9590a64b812495f0
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-09-09 10:05:59 -0700

    prevent cloudflare dns issues in test/dev

--- src/server/dns.js
+++ src/server/dns.js
@@ -11,27 +11,45 @@ function cf() {
   return cloudflare.createClient(config.cloudflare)
 }
 
-module.exports = {
-  subdomain: function subdomain(username) {
-    return product.slug+'-'+username
-  },
-  fqdn: function(subdomain) {
-    return subdomain+'.'+zone_name
-  },
-  addRecord: function(name, ip, cb) {
-    return cf().addDomainRecord(zone_name, {
-      type: 'A',
-      content: ip,
-      name: name
-    }, cb)
-  },
-  deleteRecord: function(name, cb) {
-    cf().listDomainRecords(zone_name, function (err, records) {
-      if (err) return cb(err);
-      var record = _.find(records, { name: name+'.'+zone_name });
-      if (record) {
-        return cf().deleteDomainRecord(zone_name, record.rec_id, cb)
-      } else return cb(new Error('No record', name, zone_name))
-    })
-  }
+function subdomain(username) {
+  return product.slug+'-'+username
 }
+
+function fqdn(subdomain) {
+  return subdomain+'.'+zone_name
+}
+
+module.exports = {
+  production: function() { return {
+    fqdn: fqdn,
+    subdomain: subdomain,
+    addRecord: function(name, ip, cb) {
+      return cf().addDomainRecord(zone_name, {
+        type: 'A',
+        content: ip,
+        name: name
+      }, cb)
+    },
+    deleteRecord: function(name, cb) {
+      cf().listDomainRecords(zone_name, function (err, records) {
+        if (err) return cb(err);
+        var record = _.find(records, { name: name+'.'+zone_name });
+        if (record) {
+          return cf().deleteDomainRecord(zone_name, record.rec_id, cb)
+        } else return cb(new Error('No record', name, zone_name))
+      })
+    }
+  }},
+  test: function() { return {
+    fqdn: fqdn,
+    subdomain: subdomain,
+    addRecord: function (name, ip, cb) {
+      console.log('DNS Test Stub: addRecord', name, ip)
+      cb()
+    },
+    deleteRecord: function (name, cb) {
+      console.log('DNS Test Stub: deleteRecord ', name)
+      cb()
+    }
+  }}
+}[process.env.CONFIG_ENV || process.env.NODE_ENV || 'test']()
