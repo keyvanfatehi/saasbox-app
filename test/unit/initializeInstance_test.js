a685b9c754664148bb1f73beda54b0a58a192cdd
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-09-09 08:56:34 -0700

    cleanup test

--- test/unit/initializeInstance_test.js
+++ test/unit/initializeInstance_test.js
@@ -4,23 +4,25 @@ var expect = require('chai').expect
 
 var req, instance;
 
-before(function(done) {
-  instance = null;
-  req = { user: { username: 'bob' } }
-  mw(req, null, function () {
-    instance = req.user.instance;
-    done()
+describe("middleware: initializeInstance", function() {
+  before(function(done) {
+    instance = null;
+    req = { user: { username: 'bob' } }
+    mw(req, null, function () {
+      instance = req.user.instance;
+      done()
+    })
+  });
+
+  it("selects the first agent", function() {
+    expect(instance.agent).to.eq('test')
+  });
+
+  it("sets the product slug", function () {
+    expect(instance.slug).to.eq('strider')
   })
-});
 
-it("selects the first agent", function() {
-  expect(instance.agent).to.eq('test')
-});
-
-it("sets the product slug", function () {
-  expect(instance.slug).to.eq('strider')
-})
-
-it("sets the namespace to the username", function() {
-  expect(instance.namespace).to.eq('bob')
+  it("sets the namespace to the username", function() {
+    expect(instance.namespace).to.eq('bob')
+  });
 });
