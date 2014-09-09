7f89bab6faf1d5673f7533cd0732264476861be7
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-09-08 18:41:39 -0700

    remove prod config, have it ref etc

--- etc/config.js
+++ etc/config.js
@@ -17,19 +17,5 @@ module.exports = {
     mongodb: 'mongodb://localhost/saasbox-strider',
     cookie_secret: 'o3lo1s50gckp4x6rc8hzzhowmzfwp14ij1fzwc8st4oswcdi'
   },
-  production: {
-    zone: 'knban.com',
-    agents: [{
-      name: 'the-agency',
-      ip: '104.131.17.221',
-      url: 'https://agency.knban.com',
-      secret: 'ryxvmm876h2maemi2zh1bv1ntpujtt9zw2o0vgu3hy6i529t7fz9mrcva0dx6r'
-    }],
-    cloudflare: {
-      email: 'keyvanfatehi@gmail.com',
-      token: '7fea8149b27436d01540c7afe3dc14af7ef81'
-    },
-    mongodb: 'mongodb://keyvan:qnKrXGxicnhvf7ptgqcQukeEhAuzarzcxpXiqzDWVD8GjBRevr@ds059509.mongolab.com:59509/saasbox-strider',
-    cookie_secret: '02s93kh8p8wa5rk96kykjgsbnr9kke2965jo8q2zi553ik9'
-  }
+  production: require('/etc/saasbox/config.js')
 }[process.env.CONFIG_ENV || process.env.NODE_ENV || 'test']
