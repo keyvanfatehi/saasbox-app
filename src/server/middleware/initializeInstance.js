1b61a2e3ea597e8be0a766f2a1ab62b8112cab7c
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-09-08 23:15:37 -0700

    throw where agent selection logic missing

--- src/server/middleware/initializeInstance.js
+++ src/server/middleware/initializeInstance.js
@@ -2,6 +2,7 @@ var product = require('../../../product')
 var config = require('../../../etc/config')
 
 module.exports = function (req, res, next) {
+  return res.next(new Error('you need to finish the agent selector!'))
   req.user.instance = req.user.instance || {
     slug: product.slug,
     agent: config.agents[0].name,  // TODO find the best agent
