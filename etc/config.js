c576b6c2b66d701c12f1bbfaf3dcb70fb6d751a8
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-09-08 23:15:07 -0700

    change config loader and agent define style

--- etc/config.js
+++ etc/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  test: {
+  test: function() { return {
     agents: [{
       name: 'test-agent',
       url: 'http://localhost:5010',
@@ -7,15 +7,16 @@ module.exports = {
     }],
     mongodb: 'mongodb://localhost/saasbox-test',
     cookie_secret: 'session-secret'
-  },
-  development: {
-    agents: [{
-      name: 'terranova',
-      url: 'http://localhost:4000',
-      secret: 'secret'
-    }],
+  }},
+  development: function() { return {
+    agents: {
+      dev: {
+        url: 'http://localhost:4000',
+        secret: 'secret'
+      }
+    },
     mongodb: 'mongodb://localhost/saasbox-strider',
     cookie_secret: 'o3lo1s50gckp4x6rc8hzzhowmzfwp14ij1fzwc8st4oswcdi'
-  },
-  production: require('/etc/saasbox/config.js')
-}[process.env.CONFIG_ENV || process.env.NODE_ENV || 'test']
+  }},
+  production: function() { return require('/etc/saasbox/config.js') }
+}[process.env.CONFIG_ENV || process.env.NODE_ENV || 'test']()
