de4633a7cdb71a5bda60957fb787eaedce312bcf
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-09-19 16:47:23 -0700

    setup bootstrap modals

--- src/client/app.js
+++ src/client/app.js
@@ -1,6 +1,12 @@
 var Account = require('./controllers/account_controller')
   , Instance = require('./controllers/instance_controller')
 
+require('../../vendor/bootstrap-3.2.0/js/modal')
+
+window.createModal = function (cmp) {
+  return React.renderComponent(cmp, $('#modal-mountnode').get(0))
+}
+
 window.startDashboard = function() {
   var account = new Account();
   account.mountInterface('#account');
