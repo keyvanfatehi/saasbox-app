f1ad9af7918ab3aed63d9189eaebec813077f6bf
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-09-19 17:33:26 -0700

    break out modal and prevent double stripe

--- src/client/components/billing_info.js
+++ src/client/components/billing_info.js
@@ -2,49 +2,11 @@
 var stripePublicKey = require('../../../etc/stripe.pub')
 
 module.exports = function(React) {
-  var Modal = React.createClass({
-    render: function () {
-      //<button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
-      //<button type="button" className="btn btn-primary">Save changes</button>
-      var footer = (this.props.footer ? (
-        <div className="modal-footer">{this.props.footer}</div>
-      ) : '')
-      return (
-        <div className="modal fade" id="myModal" aria-hidden="true">
-          <div className="modal-dialog">
-            <div className="modal-content">
-              <div className="modal-header">
-                <button type="button" className="close" onClick={this.hide}><span aria-hidden="true">&times;</span><span className="sr-only">Close</span></button>
-                <h4 className="modal-title">{this.props.title}</h4>
-              </div>
-              <div className="modal-body">{this.props.body}</div>
-              {footer}
-            </div>
-          </div>
-        </div>
-      )
-    },
-    setBody: function (body) {
-      this.setProps({ body: body });
-    },
-    hide: function () {
-      $(this.getDOMNode()).modal('hide');
-    },
-    componentDidMount: function () {
-      var node = this.getDOMNode()
-      $(node).on('hidden.bs.modal', function () {
-        React.unmountComponentAtNode($(node).parent().get(0))
-      })
-    },
-    show: function () {
-      var $el = $(this.getDOMNode())
-      setTimeout(function () {
-        $el.modal('show');
-      }, 500); // delay to mitigate animation glitches
-    }
-  });
-
+  var Modal = require('./modal')(React);
   var BillingInfo = React.createClass({
+    getInitialState: function () {
+      return { stripeOpen: false };
+    },
     render: function() {
       var style = this.props.email ? {} : { display: 'none' }
       return (
@@ -57,8 +19,11 @@ module.exports = function(React) {
       )
     },
     beginStripeFlow: function() {
+      if (this.state.stripeOpen) return false;
+      this.setState({ stripeOpen: true });
       var account = this.props.controller;
       var amount = 123;
+      var self = this;
       account.fetch(function(data) {
         StripeCheckout.open({
           key: stripePublicKey,
@@ -68,29 +33,27 @@ module.exports = function(React) {
           amount: amount,
           panelLabel: "Verify Card",
           allowRememberMe: true,
+          closed: function () {
+            self.setState({ stripeOpen: false });
+          },
           token: function (token) {
-            console.log('finish stripe flow', token);
             account.chargeAndSaveCard(token, amount, function(err) {
-              var modal = createModal(<Modal title="Credit Card Setup" />);
+              var body = null;
               if (err) {
-                modal.setBody(
-                  <div>
-                    Your card has not been charged.
-                    <p className="alert alert-warning" 
-                      dangerouslySetInnerHTML={{
-                        __html: err.message
-                      }}
-                    />
-                  </div>
-                )
+                body = <div>
+                  Your card has not been charged.
+                  <p className="alert alert-warning" 
+                    dangerouslySetInnerHTML={{
+                      __html: err.message
+                    }}
+                  />
+                </div>
               } else {
-                modal.setBody(
-                  <div>
-                    You have been charged a small amount that will be automatically refunded.
-                  </div>
-                )
+                body = <div>
+                  You have been charged a small amount that will be automatically refunded.
+                </div>
               }
-              modal.show()
+              createModal(<Modal title="Credit Card Setup" body={body} />);
             })
           }
         })
---
+++ src/client/components/modal.js
@@ -0,0 +1,37 @@
+/** @jsx React.DOM */
+module.exports = function(React) {
+  var Modal = React.createClass({
+    render: function () {
+      //<button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
+      //<button type="button" className="btn btn-primary">Save changes</button>
+      var footer = (this.props.footer ? (
+        <div className="modal-footer">{this.props.footer}</div>
+      ) : '')
+      return (
+        <div className="modal" id="myModal" aria-hidden="true">
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <button type="button" className="close" onClick={this.hide}><span aria-hidden="true">&times;</span><span className="sr-only">Close</span></button>
+                <h4 className="modal-title">{this.props.title}</h4>
+              </div>
+              <div className="modal-body">{this.props.body}</div>
+              {footer}
+            </div>
+          </div>
+        </div>
+      )
+    },
+    hide: function () {
+      $(this.getDOMNode()).modal('hide');
+    },
+    componentDidMount: function () {
+      var node = this.getDOMNode()
+      $(node).on('hidden.bs.modal', function () {
+        React.unmountComponentAtNode($(node).parent().get(0))
+      })
+      $(this.getDOMNode()).modal('show');
+    }
+  });
+  return Modal;
+}
