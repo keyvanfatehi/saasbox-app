286fcd0e0aa150d02d8ed7e9c70140c8f3659816
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-09-20 19:01:27 -0700

    add jsyaml and setup yaml config

--- etc/config.js
+++ etc/config.js
@@ -1,32 +1,13 @@
+var jsyaml = require('js-yaml')
+var fs = require('fs')
+var path = require('path')
+
 module.exports = {
-  test: function() { return {
-    zone: "example.com",
-    mongodb: 'mongodb://localhost/saasbox-test',
-    redis: {
-      host: "127.0.0.1",
-      port: 6379,
-      options: {
-        // Options listed at https://github.com/mranney/node_redis
-        auth_pass: null // Set it to the password or leave it as null which is default
-      }
-    },
-    cookie_secret: 'session-secret',
-    mail: {
-      transport: {
-        service: 'Mailgun',
-        auth: {
-          user: 'postmaster@knban.com',
-          pass: 'fb8e2311444a3d03301a30e43caef236'
-        }
-      }
-    },
-    mailhide: {
-      publicKey: '01zlt8r3De11zvokfkp1SA9A==',
-      privateKey: '9aa1f2990e57bfef18e90b90a064578c'
-    },
-    digitalocean: {
-      token: 'ecf230d45015b9864cd7212fe688f0bfd3b4771074b2449298642bc761ff94e0'
-    }
-  }},
-  production: function() { return require('/etc/saasbox/config.js') }
+  test: function() {
+    var configPath = path.join(__dirname, 'config.yml')
+    return jsyaml.load(fs.readFileSync(configPath))
+  },
+  production: function() {
+    return require('/etc/saasbox/config.js')
+  }
 }[(process.env.CONFIG_ENV || process.env.NODE_ENV) === 'production' ? 'production' : 'test']()
