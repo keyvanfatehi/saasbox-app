8a7cd7a4d3c91a7792cf53a9d8424b48a90da2dd
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-09-20 19:02:09 -0700

    simplify dns

--- src/server/dns.js
+++ src/server/dns.js
@@ -19,36 +19,22 @@ function fqdn(subdomain) {
 }
 
 module.exports = {
-  production: function() { return {
-    fqdn: fqdn,
-    subdomain: subdomain,
-    addRecord: function(name, ip, cb) {
-      return cf().addDomainRecord(zone_name, {
-        type: 'A',
-        content: ip,
-        name: name
-      }, cb)
-    },
-    deleteRecord: function(fqdn, cb) {
-      cf().listDomainRecords(zone_name, function (err, records) {
-        if (err) return cb(err);
-        var record = _.find(records, { name: fqdn });
-        if (record) {
-          return cf().deleteDomainRecord(zone_name, record.rec_id, cb)
-        } else return cb(new Error('No record '+fqdn))
-      })
-    }
-  }},
-  test: function() { return {
-    fqdn: fqdn,
-    subdomain: subdomain,
-    addRecord: function (name, ip, cb) {
-      console.log('DNS Test Stub: addRecord', name, ip)
-      cb()
-    },
-    deleteRecord: function (name, cb) {
-      console.log('DNS Test Stub: deleteRecord ', name)
-      cb()
-    }
-  }}
-}[(process.env.CONFIG_ENV || process.env.NODE_ENV) === 'production' ? 'production' : 'test']()
+  fqdn: fqdn,
+  subdomain: subdomain,
+  addRecord: function(name, ip, cb) {
+    return cf().addDomainRecord(zone_name, {
+      type: 'A',
+      content: ip,
+      name: name
+    }, cb)
+  },
+  deleteRecord: function(fqdn, cb) {
+    cf().listDomainRecords(zone_name, function (err, records) {
+      if (err) return cb(err);
+      var record = _.find(records, { name: fqdn });
+      if (record) {
+        return cf().deleteDomainRecord(zone_name, record.rec_id, cb)
+      } else return cb(new Error('No record '+fqdn))
+    })
+  }
+}
