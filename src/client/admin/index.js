8ef9afe0b58e3582e8e563dfa5f83918b2e13215
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-09-25 23:33:02 -0700

    setup for more fractal ui building

--- src/client/admin/index.js
+++ src/client/admin/index.js
@@ -2,7 +2,7 @@ var Jobs = require('./controllers/jobs_controller')
 
 module.exports = window
 
-window.startAdminDashboard = function() {
+window.startJobsInterface = function() {
   var jobs = new Jobs();
   var el = $('#jobs').get(0);
   jobs.mountInterface(el);
--- src/server/routers/web/admin.js
+++ src/server/routers/web/admin.js
@@ -1,5 +1,19 @@
 module.exports = function(r) {
+
+  r.use(function(req, res, next) {
+    var _render = res.render.bind(res)
+    res.render = function(tmpl) {
+      _render('admin/'+tmpl, { layout: 'layouts/admin' });
+    }
+    next()
+  })
+
+
   r.get('/admin', function(req, res) {
-    res.render('admin/dashboard');
+    res.render('dashboard');
+  })
+
+  r.get('/admin/jobs', function(req, res) {
+    res.render('jobs')
   })
 }
