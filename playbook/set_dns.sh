a70ad60c8ccaa4f4af155bd28013ed1ef239a328
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-09-30 00:51:12 -0700

    update scripts

--- playbook/set_dns.sh
+++ playbook/set_dns.sh
@@ -5,9 +5,14 @@ USERNAME="postmaster@pillbox.io"
 PASSWORD="RgcvD4KYW2DgQgbL"
 
 function setRecord() {
-  echo "$1 => $2"
-  curl -d "" --user $USERNAME:$PASSWORD https://$BOX/admin/dns/set/$1/a/$2
+  echo "$1 $2 => $3"
+  curl -d "" --user $USERNAME:$PASSWORD "https://$BOX/admin/dns/set/$2/$1/$3"
 }
 
-setRecord db.pillbox.io 104.131.100.139
-setRecord web01.pillbox.io 104.131.100.128
+# Database
+setRecord A db.pillbox.io 104.131.100.139
+
+# Web app
+setRecord A web01.pillbox.io 104.131.100.128
+
+setRecord A app.pillbox.io 104.131.100.128 # can later replace with a load balancer
