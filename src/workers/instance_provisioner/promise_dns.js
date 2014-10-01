feb78fe3e6c1186e9eeb79d2a7fa2cee677d40a2
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-09-30 21:21:34 -0700

    get ready for dns provider change

--- src/workers/instance_provisioner/promise_dns.js
+++ src/workers/instance_provisioner/promise_dns.js
@@ -2,15 +2,18 @@ var Promise = require('bluebird')
   , logger = require('../../logger')
   , cf = require('../../server/dns')
 
+//TODO DONT USE CF. USE MAILINATOR / NEEDLE
+
 module.exports = function(options) {
   return new Promise(function(resolve, reject) {
-    cf.addRecord(options.fqdn, options.ip, function(err) {
-      if (err) return reject(err);
-      else {
-        logger.info(options.fqdn+' now resolves to '+options.ip)
-        resolve();
-      }
-    })
+    //cf.addRecord(options.fqdn, options.ip, function(err) {
+    //  if (err) return reject(err);
+    //  else {
+    //    logger.info(options.fqdn+' now resolves to '+options.ip)
+    //    resolve();
+    //  }
+    //})
+    resolve()
   }).error(onError).catch(onError)
 }
 function onError(err) {
