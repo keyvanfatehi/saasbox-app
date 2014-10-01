0a63f1d7ad11c3cf431e8819fea7a915415f2326
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-09-30 22:26:43 -0700

    move mailinabox dns stuff to external module

--- src/server/dns.js
+++ src/server/dns.js
@@ -1,10 +1,11 @@
-/*
- * We are using Mailinabox @ box.pillbox.io as our DNS provider
- */
 var config = require('../../etc/config')
-  , _ = require('lodash')
-  , needle = require('needle')
   , zone_name = config.zone
+
+var client = require('mailinabox-dns-client')({
+  host: config.dns.host,
+  email: config.dns.email,
+  password: config.dns.password
+})
 
 function subdomain(slug, username) {
   return slug+'-'+username
@@ -14,41 +15,9 @@ function fqdn(subdomain) {
   return subdomain+'.'+zone_name
 }
 
-function reqOpts() {
-  return {
-    username: config.dns.email,
-    password: config.dns.password
-  }
-}
-
-function urlFor(qname, rtype, value) {
-  var url = 'https://'+config.dns.host+'/admin/dns/set'
-  url += '/'+qname+'/'+rtype+'/'+value;
-  return url;
-}
-
-client = {
+module.exports = {
   fqdn: fqdn,
   subdomain: subdomain,
-  addRecord: function(fqdn, ip, cb) {
-    var url = urlFor(fqdn, 'a', ip)
-    return needle.post(url, '', reqOpts(), cb)
-  },
-  deleteRecord: function(fqdn, cb) {
-    var url = urlFor(fqdn, 'a', '__delete__')
-    return needle.post(url, '', reqOpts(), cb)
-  }
+  addRecord: client.addRecord,
+  deleteRecord: client.deleteRecord
 }
-
-module.exports = client;
-
-
-/* Test
-client.deleteRecord('foobar.pillbox.io', function (err, res) {
-  console.log(err, res.body)
-})
-
-client.addRecord('foobar.pillbox.io', '127.0.0.1', function (err, res) {
-  console.log(err, res.body)
-})
-*/
