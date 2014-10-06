4e2788ed18db916fa0896df6478d68e2b3cd18de
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-10-06 14:43:27 -0700

    support connecting by IP

--- bin/env_box.sh
+++ bin/env_box.sh
@@ -7,6 +7,6 @@ fi
 FQDN=$(node -e "console.log(require('./etc/config').zone)")
 USERNAME=$1
 APPNAME=$2
-HOST=$2-$1.$FQDN
+HOST=${IP_ADDRESS-$2-$1.$FQDN}
 CONTAINER=$2-$1.$2
 IDENTITY=etc/ssh/id_rsa 
