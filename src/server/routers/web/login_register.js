0c2b07125887e6699fc82428ae2d88427369e0b8
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-10-11 14:24:35 -0700

    refactor registration

--- src/server/routers/web/login_register.js
+++ src/server/routers/web/login_register.js
@@ -3,64 +3,72 @@ var Account = require('../../models').Account
   , registrationValidator = require('../../../validators/registration')
   , mw = require('../../middleware')
 
+var Promise = require('bluebird');
+Promise.promisifyAll(Account);
+Promise.promisifyAll(Account.prototype);
+
 module.exports = function(router) {
   router.get('/register', function(req, res) {
     res.render('register', {errors:[]});
   });
 
-  router.post('/register', function(req, res, next) {
-    var errors = registrationValidator(req.body)
-    if (errors.length > 0) {
-      res.render('register', { errors: errors })
-    } else {
-      Account.findOne({ email: req.body.email }).exec(function(err, dupe) {
-        if (err) return next(err);
-        if (dupe) return res.render("register", {
-          errors: [
-            "Email is already in use. Use 'forgot password' to recover your account."
-          ]
-        });
-        var token = Math.random().toString(34).substring(2)
-        var account = new Account({
-          username: req.body.username,
-          unverifiedEmail: req.body.email,
-          unverifiedEmailToken: token
-        })
-        Account.register(account, req.body.password, function(err, account) {
-          if (err) return res.render("register", {
-            errors: [
-              "Username is already in use."
-            ]
-          });
-          else return passport.authenticate('local')(req, res, function () {
-            var host = req.protocol+'://'+req.get('host') 
-            // send a welcome email 
-            account.sendNewUserWelcomeEmail({
-              url: host+'/verify_email?token='+token
-            })
-            req.flash('info', 'Thank you for signing up. We have sent a confirmation email to '+req.body.email)
-          });
-        });
-      })
-    }
-  });
-
-  router.get('/verify_email', function(req, res, next) {
-    Account.verifyEmailByToken(req.query.token, function(err) {
-      if (err) req.flash('error', 'That appears to be an invalid confirmation code.');
-      else req.flash('info', 'Thank you for confirming your email address!');
-      res.redirect('/')
-    })
-  })
-
   router.get('/login', function(req, res) {
     res.render('login', { user : req.user });
   });
 
-  router.post('/login', passport.authenticate('local'), mw.loginRequired.afterLogin);
+  router.post('/login', passport.authenticate('local', {
+    failureRedirect: '/login',
+    failureFlash: true
+  }), mw.loginRequired.afterLogin);
 
   router.get('/logout', function(req, res) {
     req.logout();
     res.redirect('/');
   });
+
+  router.get('/verify_email', function(req, res, next) {
+    Account.verifyEmailByToken(req.query.token, function(err) {
+      if (err) req.flash('error', 'Confirmation code has expired.');
+      else req.flash('info', 'Thank you for confirming your email address!');
+      res.redirect('/')
+    })
+  })
+
+  router.post('/register', function(req, res, next) {
+    var flashBack = function(err) {
+      console.log('flashback', err);
+      req.flash('error', err.message);
+      return res.redirect('back');
+    }
+    var errors = registrationValidator(req.body)
+    if (errors.length) return flashBack(errors[0]);
+    Account.findOneAsync({ email: req.body.email }).then(function(dupe) {
+      if (dupe) throw new Error("Email is already in use. Use 'forgot password' to recover your account.")
+      return Account.findOneAsync({ username: req.body.username })
+    }).then(function(dupe) {
+      console.log(dupe);
+      if (dupe) throw new Error('Username is already in use');
+      // delete any accounts that were also planning to use this email address
+      return Account.removeAsync({ unverifiedEmail: req.body.email })
+    }).then(function(num) {
+      console.log('deleted ', num);
+      var token = Math.random().toString(34).substring(2)
+      var account = new Account({
+        username: req.body.username,
+        unverifiedEmail: req.body.email,
+        unverifiedEmailToken: token
+      })
+      return Account.registerAsync(account, req.body.password)
+    }).then(function(account) {
+      return passport.authenticate('local')(req, res, function () {
+        var host = req.protocol+'://'+req.get('host') 
+        // send a welcome email 
+        account.sendNewUserWelcomeEmail({
+          url: host+'/verify_email?token='+account.unverifiedEmailToken
+        })
+        req.flash('info', 'Thank you for signing up. We have sent a confirmation email to '+req.body.email)
+        next()
+      });
+    }).error(flashBack).catch(flashBack)
+  }, mw.loginRequired.afterLogin);
 }
