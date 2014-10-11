32c79f1d792226aba62755635327f69db5365af3
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-10-11 11:45:57 -0700

    simplify product index

--- products/index.js
+++ products/index.js
@@ -1,12 +1,3 @@
-var _ = require('lodash')
-
-var apps = [
-  require('./sentry'),
-  require('./strider')
-]
-
-var active = _.where(apps, {
-  inDevelopment: undefined
-})
-
-module.exports = _.zipObject(_.pluck(active, 'slug'), active)
+module.exports = {
+  strider: require('./strider')
+}
