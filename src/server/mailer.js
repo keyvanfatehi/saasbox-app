c2095f2270b4d45eb3efd7064679732d88b34ca3
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-10-12 13:37:57 -0700

    enable mailer to render mail templates

--- src/server/mailer.js
+++ src/server/mailer.js
@@ -1,11 +1,15 @@
-var config = require('../../etc/config')
-  , nodemailer = require('nodemailer')
+var fs = require('fs')
+  , ejs = require('ejs')
+  , path = require('path')
   , logger = require('../logger')
+  , nodemailer = require('nodemailer')
+  , config = require('../../etc/config')
+  , emailTemplates = __dirname+'../../views/emails'
   , transport = nodemailer.createTransport(config.email)
 
 module.exports = {
   sendMail: function(opts, callback) {
-    logger.info(opts);
+    logger.info('send email', opts);
     if (!opts.subject) throw new Error('no mail subject')
     if (!opts.to) throw new Error('no mail recipients')
     if (!opts.text) throw new Error('no mail body')
@@ -15,9 +19,16 @@ module.exports = {
       to: opts.to,
       text: opts.text
     }, function(error, info) {
-      if (error) logger.error(error.message);
+      if (error) logger.error('error sending email', {
+        type: 'mail',
+        stack: error.stack
+      });
       else logger.info('Message sent: ' + info.response);
       if (callback) callback(error, info);
     })
+  },
+  renderTemplate: function(name, locals) {
+    var tmpl = path.join(emailTemplates, name);
+    return ejs.render(fs.readFileSync(tmpl), locals)
   }
 }
