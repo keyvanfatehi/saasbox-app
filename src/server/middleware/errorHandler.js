4d0b521b97db3b088ab081c8588f34469d946fcf
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-10-12 14:47:18 -0700

    implement validation error handling

--- src/server/middleware/errorHandler.js
+++ src/server/middleware/errorHandler.js
@@ -1,23 +1,42 @@
 var logger = require('../../logger')
 
 module.exports = function(err, req, res, next) {
-  var logData = {
-    type: 'web',
-    url: req.originalUrl,
-    method: req.method,
-    stack: err.stack,
-    user: (req.user ? {
-      _id: req.user._id.toString(),
-      username: req.user.username,
-      email: req.user.email
-    } : null)
-  }
-  logger.error(err.message, logData)
-  res.status(500)
-  console.log(req.url);
-  if (/^\/api\/v\d/.test(req.url)) {
-    res.json({ error: "Internal server error" })
+  var apiRequest = /^\/api\/v\d/.test(req.url);
+  if (err.name === "ValidationError") {
+    var messages = []
+    for (var key in err.errors)
+      messages.push(err.errors[key].message)
+
+    if (apiRequest) {
+      req.status(400)
+      return res.json({
+        error: 'ValidationError',
+        messages: messages
+      });
+    } else {
+      messages.forEach(function(message) {
+        req.flash('error', message)
+      })
+      return res.redirect('back')
+    }
   } else {
-    res.render('internal_error');
+    var logData = {
+      type: 'web',
+      url: req.originalUrl,
+      method: req.method,
+      stack: err.stack,
+      user: (req.user ? {
+        _id: req.user._id.toString(),
+        username: req.user.username,
+        email: req.user.email
+      } : null)
+    }
+    logger.error(err.message, logData)
+    res.status(500)
+    if (apiRequest) {
+      res.json({ error: "Internal server error" })
+    } else {
+      res.render('internal_error');
+    }
   }
 }
