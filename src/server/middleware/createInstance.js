2101115a50a726045d5e70db7553a40ff5bbfbe8
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-10-12 15:55:34 -0700

    pay up the first hour on create instance

--- src/server/middleware/createInstance.js
+++ src/server/middleware/createInstance.js
@@ -2,7 +2,8 @@ var createInstance = require('../create_instance')
   , priceMatrix = require('../../../etc/price_matrix')
   , regions = require('../../../etc/regions')
   , cloudProviders = require('../../cloud_providers')
-  , Instance = require('../models').Instance;
+  , Instance = require('../models').Instance
+  , instanceCost = require('../../instance_cost')
 
 module.exports = function (req, res, next) {
   var problems = []
@@ -11,30 +12,29 @@ module.exports = function (req, res, next) {
     account: req.user._id
   })
 
-  var product = instance.getProduct()
-
-  if (product.inDevelopment)  {
-    problems.push(product.title+" hosting is currently in development. You will receive an email from us when it becomes available.")
-  }
+  var size = priceMatrix[req.body.size]
+  var firstHourCost = instanceCost(size.cents).hourly.cents
 
   if (!req.user.email)  {
-    problems.push('You must verify your email address')
+    problems.push('You must confirm your email address')
   }
 
-  if (!req.user.isBillingOk())
+  if (!req.user.isBillingOk({ cents: firstHourCost }))
     problems.push('Please setup your billing information')
-
 
   if (problems.length > 0)
     res.status(403).json({ problems: problems })
   else {
     var cloud = cloudProviders[req.body.cloud]
-    var spec = priceMatrix[req.body.size]
     var region = regions[req.body.region]
-    if (cloud && spec && region) {
-      createInstance(req.user, instance, req.body.cloud, spec, req.body.region, function(err, instance) {
+    if (cloud && size && region) {
+      createInstance(req.user, instance, req.body.cloud, size, req.body.region, function(err, instance) {
         req.instance = instance;
-        next()
+        req.user.balance += firstHourCost;
+        req.user.save(function(err) {
+          if (err) return next(err);
+          next()
+        })
       })
     } else {
       res.status(400).end('Unacceptable payload')
