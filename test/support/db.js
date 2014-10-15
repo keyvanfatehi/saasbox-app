78cfe5246a1fa724912e0e441eee26c93857b41b
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-10-15 07:08:18 -0700

    improve db support

--- test/support/db.js
+++ test/support/db.js
@@ -6,14 +6,32 @@ mongoose.connection.on('error', function(err) {
   throw err;
 })
 
+var connected = false;
+
+var connect = function(done) {
+  if (connected) return done();
+  mongoose.connection.on('connected', function() {
+    connected = true;
+    done()
+  })
+}
+
+var truncate = function(done) {
+  mongoose.connection.db.dropDatabase(function(err) {
+    if (err) throw err;
+    done(err)
+  })
+}
+
+// connect and truncate
+var setup = function(done) {
+  connect(function() {
+    truncate(done);
+  });
+}
+
 module.exports = {
-  connect: function(done) {
-    mongoose.connection.on('connected', done)
-  },
-  truncate: function(done) {
-    mongoose.connection.db.dropDatabase(function(err) {
-      if (err) throw err;
-      done(err)
-    })
-  }
+  connect: connect,
+  truncate: truncate,
+  setup: setup
 }
