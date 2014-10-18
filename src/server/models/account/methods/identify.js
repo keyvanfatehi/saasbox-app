ab1f58ce70abb8133bc9b40737bf12e6c23bb6e1
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-10-18 07:22:47 -0700

    fix account identify

--- src/server/models/account/methods/identify.js
+++ src/server/models/account/methods/identify.js
@@ -1,10 +1,13 @@
 var analytics = require('../../../../analytics')
 
 module.exports = function() {
-  analytics.identify(this._id, {
-    username: this.username,
-    email: this.email || this.unverifiedEmail,
-    loginCount: this.logins,
-    isBillingOk: this.isBillingOk()
+  analytics.identify({
+    userId: this._id,
+    traits: {
+      username: this.username,
+      email: this.email || this.unverifiedEmail,
+      loginCount: this.logins,
+      isBillingOk: this.isBillingOk()
+    }
   })
 }
