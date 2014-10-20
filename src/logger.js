0aa0875c35bca03a06f30d4f8b1909d0d348cae6
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-10-20 12:35:42 -0700

    replace loggly with papertrail

--- src/logger.js
+++ src/logger.js
@@ -1,35 +1,41 @@
 var winston = module.exports = require('winston')
 var config = require('../etc/config')
-var prod = process.env.NODE_ENV === 'production'
-var os = require('os');
 
-if (prod) {
-  // this might have some issues
-  // we need to spend time looking more into logging
-  // but for now i just want to have logs, so we'll use this
-  require('winston-loggly')
-  winston.add(winston.transports.Loggly, {
-    subdomain: config.loggly.subdomain,
-    token: config.loggly.token,
-    tags: [os.hostname()],
-    json: true,
-    timestamp: function() {
-      return new Date().toISOString()
+var papertrailTransport = function() {
+  var Papertrail = require('winston-papertrail').Papertrail
+  var transport = new Papertrail({
+    host: config.papertrail.host,
+    port: config.papertrail.port,
+    hostname: require('os').hostname(),
+    program: require('path').basename(process.argv[1]),
+    logFormat: function(level, message) {
+      return level+': '+message
     }
   })
-} else {
-  winston.remove(winston.transports.Console);
-  if (false) {
-    winston.add(winston.transports.Console, {
-      colorize: true,
-      json: true,
-      timestamp: function() {
-        return new Date().toISOString()
-      }
-    });
-  } else {
-    winston.add(winston.transports.Console, {
-      colorize: true
-    })
-  }
+  return transport
 }
+
+var consoleColorTransport = function() {
+  var transport = new winston.transports.Console({
+    colorize: true
+  })
+  return transport;
+}
+
+var transports = []
+if (process.env.NODE_ENV === 'production') {
+  console.log('Logging to papertrail!')
+  transports.push(papertrailTransport())
+} else {
+  transports.push(consoleColorTransport())
+}
+
+module.exports = new winston.Logger({
+  transports: transports,
+  levels: {
+    debug: 0,
+    info: 1,
+    warn: 2,
+    error: 3
+  }
+})
