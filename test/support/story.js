0718eb0a9a005632d40f156924052eafb4da1ded
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-10-21 18:55:31 -0700

    update story support

--- test/support/story.js
+++ test/support/story.js
@@ -1,13 +1,21 @@
 /* cucumber style story runner for mocha */
 module.exports = function(config) {
-  return function(beforeSteps, its, afterSteps) {
-    describe(beforeSteps.join(', '), function() {
+  var storyBodyFunction = function(beforeSteps, its, afterSteps) {
+    return function() {
       beforeSteps.forEach(function(func, i) {
         beforeEach(function(done) {
           loader(config, beforeSteps)[i](done)
         })
       })
+
+      if (config.beforeAssertions)
+        config.beforeAssertions()
+
       its();
+
+      if (config.afterAssertions)
+        config.afterAssertions()
+
       if (afterSteps && afterSteps.length) {
         afterSteps.forEach(function(func, i) {
           afterEach(function(done) {
@@ -15,8 +23,22 @@ module.exports = function(config) {
           })
         })
       }
-    })
+
+    }
   }
+  var _build = function(fn, _before, _its, _after) {
+    fn(_before.join(', '), storyBodyFunction(_before, _its, _after))
+  }
+  var storyBuilder = function(before, its, after) {
+    _build(describe, before, its, after)
+  }
+  storyBuilder.only = function(before, its, after) {
+    _build(describe.only, before, its, after)
+  }
+  storyBuilder.skip = function(before, its, after) {
+    _build(describe.skip, before, its, after)
+  }
+  return storyBuilder;
 }
 
 var loader = function(config, steps) {
