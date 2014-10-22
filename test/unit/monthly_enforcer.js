3d5c86bd3c0711dfcc3dfe0f0ba3533cec58b716
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-10-21 19:00:47 -0700

    update monthly enforcer test

--- test/unit/monthly_enforcer.js
+++ test/unit/monthly_enforcer.js
@@ -6,17 +6,26 @@ var models = require('../../src/server/models')
   , sinon = require('sinon')
   , setupDB = require('../support/db').setup
   , enforcerSupport = require('../support/enforcer')
-  , afterTick = enforcerSupport.afterTick('monthly')
   , storySupport = require('../support/story')
   , mailer = require('../../src/server/mailer')
   , Queues = require('../../src/queues')
+  , stripe = require('../../src/server/billing/stripe')
 
 chai.use(require('sinon-chai'))
 
 describe("monthly enforcer", function() {
+  var account = null;
   var story = storySupport({
     getContext: enforcerSupport.getContext,
-    getSteps: enforcerSupport.steps
+    getSteps: enforcerSupport.steps,
+    beforeAssertions: function() {
+      beforeEach(function(done) {
+        enforcerSupport.tick('monthly', function(err, _account) {
+          account = _account
+          done();
+        })
+      });
+    }
   })
 
   beforeEach(function(done) {
@@ -34,14 +43,30 @@ describe("monthly enforcer", function() {
   });
 
   story([
-    "account owes money",
-    "account billing is ok"
+    "account owes money"
   ], function() {
-    it.skip("charges the card on file", afterTick(function(account) {
-    }));
+    beforeEach(function() {
+      sinon.stub(stripe, 'createCharge')
+    });
 
-    it.skip("updates the balance to 0", afterTick(function(account) {
+    afterEach(function() {
+      stripe.createCharge.restore()
+    });
+
+    it("charges the card on file", function() {
+      expect(stripe.createCharge.callCount).to.eq(1);
+    })
+
+    it("updates the balance to 0", function() {
       expect(account.balance).to.eq(0)
-    }));
+    })
+  })
+
+  story.skip([
+    "account does not owe any money"
+  ], function() {
+    it("does not charge the card on file", function() {
+      expect(stripe.createCharge.callCount).to.eq(0);
+    })
   })
 })
