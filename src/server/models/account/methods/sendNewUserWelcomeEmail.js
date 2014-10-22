fb1547b2092b46f599ca916b12ff361a97cd4cc6
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-10-22 14:43:01 -0700

    use template for welcome email

--- src/server/models/account/methods/sendNewUserWelcomeEmail.js
+++ src/server/models/account/methods/sendNewUserWelcomeEmail.js
@@ -5,10 +5,9 @@ module.exports = function(opts, cb) {
   mailer.sendMail({
     to: this.unverifiedEmail,
     subject: 'Welcome! Please verify your email address',
-    text: [
-      'Thank you for creating a '+config.app_name+' account',
-      'Please click here to verify your email address:',
-      opts.url
-    ].join('\n')
+    text: mailer.renderTemplate('welcome_new_user', {
+      username: this.username,
+      verificationURL: opts.url
+    })
   }, cb)
 }
