1ffbee4a2196aecc4398c3713961e97cb0d03016
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-10-23 11:31:36 -0700

    remove shell for now

--- products/index.js
+++ products/index.js
@@ -3,7 +3,7 @@ var productPricing = require('../src/product_pricing')
 var products = {
   'strider-1.5.0': require('./strider-1.5.0'),
   'strider-1.6.0': require('./strider-1.6.0'),
-  'shell': require('./shell')
+//  'shell': require('./shell') solve the digicert/ssl/shit first
 }
 
 for (var key in products) {
