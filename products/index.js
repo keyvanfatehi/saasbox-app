54fefab8c6e7dbf556d77781f47bc576d8183851
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-10-25 20:21:34 -0700

    situate product drops

--- products/index.js
+++ products/index.js
@@ -1,9 +1,8 @@
 var productPricing = require('../src/product_pricing')
 
 var products = {
-  'strider-1.5.0': require('./strider-1.5.0'),
-  'strider-1.6.0': require('./strider-1.6.0'),
-//  'shell': require('./shell') solve the digicert/ssl/shit first
+  'strider': require('./strider')
+//  'shell': require('./shell') // solve the digicert/ssl/shit XXX
 }
 
 for (var key in products) {
