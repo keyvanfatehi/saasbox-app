e0e228ee6bed4eb7416dca4daef6b2cbea161ba5
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-10-25 20:21:01 -0700

    delete agent

--- agent/src/server/routes/index.js
+++
@@ -1,8 +0,0 @@
-var version = require('../../../package').version;
-var router = require('express').Router()
-router.get('/version', function(req, res) {
-  res.json({ version: version });
-})
-require('./proxies')(router)
-require('./drops')(router)
-module.exports = router
