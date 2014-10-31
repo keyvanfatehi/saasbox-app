132b4fa1710041ba7605c4b93ad1a41df940904b
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-10-31 10:28:58 +0100

    remove a console log

--- src/server/reconfigure_instance.js
+++ src/server/reconfigure_instance.js
@@ -2,7 +2,6 @@ var Queue = require('../queues').instanceConfigurator
   , logger = require('../logger')
 
 module.exports = function(instance, config, done) {
-  console.log(config)
   instance.config = config
   instance.save(function (err) {
     if (err) return done(err);
